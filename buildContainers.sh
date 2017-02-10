d1b092568b8390195c11f40b16af1fa972ba38d4
Alan Harnum <waharnum@gmail.com>
2017-02-10 14:45:38 -0500

    Initial commit.

---
+++ buildContainers.sh
@@ -0,0 +1,4 @@
+docker build -t gpiilab/nodejs node/.
+docker build -t gpiilab/universal universal/.
+docker build -t gpiilab/prefserver prefserver/.
+docker build -t gpiilab/dataloader dataloader/.
