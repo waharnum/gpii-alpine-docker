08d91478cecb2a03598d3d54ba8b901068eec9f0
Alan Harnum <waharnum@gmail.com>
2017-02-10 14:53:26 -0500

    Correct tag name.

--- buildContainers.sh
+++ buildContainers.sh
@@ -1,4 +1,4 @@
 docker build -t gpiilab/nodejs node/.
 docker build -t gpiilab/universal universal/.
-docker build -t gpiilab/prefserver prefserver/.
+docker build -t gpiilab/preferences-server prefserver/.
 docker build -t gpiilab/dataloader dataloader/.
