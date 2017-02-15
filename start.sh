1a9a93f92c1efec658da7c63dbdd539b1609ed0b
Alan Harnum <waharnum@gmail.com>
2017-02-15 09:42:04 -0500

    Add flowManager.

--- start.sh
+++ start.sh
@@ -2,8 +2,9 @@
 
 # clean up
 kubectl delete deployment gpii-deployment
-kubectl delete service gpii-service-preferences-server
 kubectl delete service gpii-service-couchdb
+kubectl delete service gpii-service-preferences-server
+kubectl delete service gpii-service-flow-manager
 kubectl delete job dataloader
 
 # use minikube's docker instance
@@ -13,6 +14,7 @@ eval $(minikube docker-env)
 docker build -t gpiilab/nodejs node/.
 docker build -t gpiilab/universal universal/.
 docker build -t gpiilab/preferences-server prefserver/.
+docker build -t gpiilab/flow-manager flowmanager/.
 docker build -t gpiilab/dataloader dataloader/.
 
 # create the deployment and service
@@ -20,12 +22,11 @@ docker build -t gpiilab/dataloader dataloader/.
 kubectl create -f gpii-deployment.yaml
 kubectl create -f gpii-service-preferences-server.json
 kubectl create -f gpii-service-couchdb.json
+kubectl create -f gpii-service-flow-manager.json
 
-echo "Preferences Server available at:"
-echo $(minikube service gpii-service-preferences-server --url)
-
-echo "CouchDB available at:"
-echo $(minikube service gpii-service-couchdb --url)
+couchDBAddress=$(minikube service gpii-service-couchdb --url)
+preferencesServerAddress=$(minikube service gpii-service-preferences-server --url)
+flowManagerAddress=$(minikube service gpii-service-flow-manager --url)
 
 echo "Launching dataloader job"
 kubectl create -f gpii-job-dataloader.yaml
@@ -38,6 +39,18 @@ kubectl logs $dataloaderPod -f
 echo "Giving dataloader job time to complete... (crude, I know)"
 sleep 30
 
-echo "CURLIing Carla"
+echo "Getting (via curl) carla test user's preferences from Preference Server"
 curl $(minikube service gpii-service-preferences-server --url)/preferences/carla
 echo
+
+echo "Getting (via curl) carla test user's org.gnome.desktop.a11y.magnifier preference from Flow Manager"
+curl -g $(minikube service gpii-service-flow-manager --url)/carla/settings/%7B%22OS%22:%7B%22id%22:%22linux%22%7D,%22solutions%22:[%7B%22id%22:%22org.gnome.desktop.a11y.magnifier%22%7D]%7D
+echo
+
+echo "CouchDB available at: $couchDBAddress"
+
+echo "Preferences Server available at:"
+echo $preferencesServerAddress
+
+echo "Flow Manager available at:"
+echo $flowManagerAddress
